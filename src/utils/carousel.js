b069d969c5de545a7331315b16bcf69f6268bb6b
nate-d3v <nate.d3v@gmail.com>
2023-06-30 23:54:17 -0300

    Add carousel

---
+++ src/utils/carousel.js
@@ -0,0 +1,53 @@
+import { Carousel } from 'flowbite';
+
+const items = [
+	{
+		position: 0,
+		el: document.getElementById('carousel-item-1'),
+	},
+	{
+		position: 1,
+		el: document.getElementById('carousel-item-2'),
+	},
+	{
+		position: 2,
+		el: document.getElementById('carousel-item-3'),
+	},
+	{
+		position: 3,
+		el: document.getElementById('carousel-item-4'),
+	},
+];
+
+const options = {
+	defaultPosition: 1,
+	interval: 7000,
+
+	indicators: {
+		activeClasses: 'bg-white dark:bg-gray-800',
+		inactiveClasses:
+			'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+		items: [
+			{
+				position: 0,
+				el: document.getElementById('carousel-indicator-1'),
+			},
+			{
+				position: 1,
+				el: document.getElementById('carousel-indicator-2'),
+			},
+			{
+				position: 2,
+				el: document.getElementById('carousel-indicator-3'),
+			},
+			{
+				position: 3,
+				el: document.getElementById('carousel-indicator-4'),
+			},
+		],
+	},
+};
+
+const carousel = new Carousel(items, options);
+
+carousel.cycle();
