d30c91234066f5e9a7ae84395706db4d627deecd
nate-d3v <nate.d3v@gmail.com>
2023-07-04 22:42:06 -0300

    Add yaml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import image from '@astrojs/image';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), image()],
+	site: 'https://Ethereum-Chile.github.io',
+	base: '/sitio-web',
 });
